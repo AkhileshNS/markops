cd9cc3ca10fe664a82fa01b9cf5b05f70ecd4f8a
AkhileshNS <nsakhilesh02@gmail.com>
2019-06-23 19:11:08 +0530

    Completed test utility for Sidebar

--- src/App/components/Sidebar/Sidebar.test.js
+++ src/App/components/Sidebar/Sidebar.test.js
@@ -3,15 +3,42 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 // Component to test
-import { Sidebar } from './Sidebar'
+import { Sidebar } from './Sidebar';
+import {
+  SidebarContainer,
+  List,
+  ListItem,
+  ListItemTitle
+} from './Sidebar.styles';
 
-describe('<Sidebar />',() => {
- let wrapper;
- beforeEach(() => {
-  wrapper = shallow(<Sidebar />);
- })
+describe('<Sidebar />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Sidebar />);
+  });
 
- it('should have atleast one test', () => {
-  expect(true).toBeTruthy();
- });
-})
+  it('should render a list of dates', () => {
+    wrapper.setProps({
+      dates: ["AY 2016-2017","AY 2017-2018"]
+    })
+    expect(wrapper.matchesElement(<SidebarContainer>
+      <List>
+        <ListItem selected>
+          <ListItemTitle>AY 2016-2017</ListItemTitle>
+        </ListItem>
+        <ListItem>
+          <ListItemTitle>AY 2017-2018</ListItemTitle>
+        </ListItem>
+      </List>
+    </SidebarContainer>)).toBeTruthy();
+  });
+
+  it('should set "selected" as true in the ListItem whose index === props.selected', () => {
+    wrapper.setProps({
+      dates: ["AY 2016-2017","AY 2017-2018"],
+      selectedDate: 1
+    })
+    expect(wrapper.find(ListItem).get(0).props.selected).toBeFalsy();
+    expect(wrapper.find(ListItem).get(1).props.selected).toBeTruthy();
+  });
+});
